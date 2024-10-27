5c7353fcba738d9d340875e0fd1552fbf541bd86
Abhishek Raj <abhishek@onv8.com>
2024-10-27 21:15:35 +0530

    version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
+import Main from './components/Main/Main';
+
+const App = () => (
+  <>
+    <Sidebar />
+    <Main />
+  </>
+);
+
+export default App;
---
+++ src/config/optimus.js
@@ -0,0 +1,37 @@
+// npm install @google/generative-ai
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyDwqkE8VpIixRX6k__aN51eupFGrMA5HS0";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function runChat(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+  }
+  
+  export default runChat; 
